67d5913ede59ea971413aecfe3b73b8b5df2107d
Thijs <lowette@gmail.com>
2022-03-03 14:18:23 -0500

    Add placeholder example for Verimatrix Multi-DRM Core. (#16)
    Co-authored-by: thijsl <thijs.lowette@theoplayer.com>

--- android/app/src/main/java/com/theoplayer/contentprotectionintegration/SourceManager.java
+++ android/app/src/main/java/com/theoplayer/contentprotectionintegration/SourceManager.java
@@ -57,6 +57,24 @@ public class SourceManager {
         ).build();
     }
 
+    private SourceDescription buildDefaultWidevineSourceDescription(
+            String manifestUrl,
+            String licenseUrl,
+            HashMap<String, String> headers) {
+        return sourceDescription(
+                typedSource(manifestUrl)
+                        .drm(new DRMConfiguration.Builder()
+                                .widevine(
+                                        keySystemConfiguration(licenseUrl)
+                                        .headers(headers)
+                                        .build()
+                                )
+                                .build())
+                        .build()
+        ).build();
+    }
+
+
     private void initSources(Context context) {
         // Vualto VUDRM Widevine content protect integration
         String VUDRM_ID = "VUDRM";
@@ -114,6 +132,18 @@ public class SourceManager {
                             put("x-keyos-authorization", "PEtleU9TQXV0aGVudGljYXRpb25YTUw+PERhdGE+PEdlbmVyYXRpb25UaW1lPjIwMTYtMTEtMTkgMDk6MzQ6MDEuOTkyPC9HZW5lcmF0aW9uVGltZT48RXhwaXJhdGlvblRpbWU+MjAyNi0xMS0xOSAwOTozNDowMS45OTI8L0V4cGlyYXRpb25UaW1lPjxVbmlxdWVJZD4wZmZmMTk3YWQzMzQ0ZTMyOWU0MTA0OTIwMmQ5M2VlYzwvVW5pcXVlSWQ+PFJTQVB1YktleUlkPjdlMTE0MDBjN2RjY2QyOWQwMTc0YzY3NDM5N2Q5OWRkPC9SU0FQdWJLZXlJZD48V2lkZXZpbmVQb2xpY3kgZmxfQ2FuUGxheT0idHJ1ZSIgZmxfQ2FuUGVyc2lzdD0iZmFsc2UiIC8+PFdpZGV2aW5lQ29udGVudEtleVNwZWMgVHJhY2tUeXBlPSJIRCI+PFNlY3VyaXR5TGV2ZWw+MTwvU2VjdXJpdHlMZXZlbD48L1dpZGV2aW5lQ29udGVudEtleVNwZWM+PEZhaXJQbGF5UG9saWN5IC8+PExpY2Vuc2UgdHlwZT0ic2ltcGxlIiAvPjwvRGF0YT48U2lnbmF0dXJlPk1sNnhkcU5xc1VNalNuMDdicU8wME15bHhVZUZpeERXSHB5WjhLWElBYlAwOE9nN3dnRUFvMTlYK1c3MDJOdytRdmEzNFR0eDQydTlDUlJPU1NnREQzZTM4aXE1RHREcW9HelcwS2w2a0JLTWxHejhZZGRZOWhNWmpPTGJkNFVkRnJUbmxxU21raC9CWnNjSFljSmdaUm5DcUZIbGI1Y0p0cDU1QjN4QmtxMUREZUEydnJUNEVVcVJiM3YyV1NueUhGeVZqWDhCR3o0ZWFwZmVFeDlxSitKbWI3dUt3VjNqVXN2Y0Fab1ozSHh4QzU3WTlySzRqdk9Wc1I0QUd6UDlCc3pYSXhKd1ZSZEk3RXRoMjhZNXVEQUVZVi9hZXRxdWZiSXIrNVZOaE9yQ2JIVjhrR2praDhHRE43dC9nYWh6OWhVeUdOaXRqY2NCekJvZHRnaXdSUT09PC9TaWduYXR1cmU+PC9LZXlPU0F1dGhlbnRpY2F0aW9uWE1MPg==");
                         }}
                 )
+        );
+
+        // Verimatrix Multi-DRM Core
+        sources.put(
+            "Verimatrix Multi-DRM Core",
+            buildDefaultWidevineSourceDescription(
+                "https://example.com/stream.mpd",
+                "https://multidrm.vsaas.verimatrixcloud.net/widevine",
+                new HashMap<String, String>() {{
+                    put("authorization", "ey...");
+                }}
+            )
         );
 
         // add other registrations & sources here ...
